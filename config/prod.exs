164b33df4aae4f14d83f3c61c8ba2c15523386bc
Minh Khang <luongminhkhang@gmail.com>
2019-10-11 22:08:14 +0700

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,10 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :twitter, TwitterWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "phoenixtwitter", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
+
 
 # Do not print debug messages in production
 config :logger, level: :info
